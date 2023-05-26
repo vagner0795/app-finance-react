b567744f1f3c25502c87e936a7140b2d3bffa8d7
vagner0795 <vagner-se@hotmail.com>
2023-05-26 09:58:05 -0300

    feat(app-finance): first commit app-finance, using React, Nextjs. Typescript and Tailwind
    git branch -M master

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import GlobalStyle from '../components/globalstyles'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/globalStyle'
+import { theme } from '../styles/theme'
+import '../styles/globals.css'
 
-const theme: DefaultTheme = {
-  colors: {
-    primary: '#111',
-    secondary: '#0070f3',
-  },
-}
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
