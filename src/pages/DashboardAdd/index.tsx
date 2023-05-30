29a82eb53e9a26ab4ed4973b70eb999fd067db51
vagner0795 <vagner-se@hotmail.com>
2023-05-30 15:23:42 -0300

    feat(update): created add expensive page and remove files components

--- src/pages/DashboardAdd/index.tsx
+++ src/pages/DashboardAdd/index.tsx
@@ -6,13 +6,15 @@ import { TemplateContainer, TemplateContent, TemplateHeroSideBar, TemplateMain }
 
 export const DashboardAdd = () => {
   return (
-    <TemplateContainer>
+    <TemplateContainer className='h-full md:h-screen py-4'>
+        
         <TemplateContent>
-            <TemplateHeroSideBar >
-                <img className='h-40 md:h-96 my-8 md:my-0' src="" alt="image-login" />
+            <TemplateHeroSideBar>
+            <img className='h-32 md:h-80 my-8 md:my-0 rounded-lg' src="../expensive.png" alt="image-login" />
+
             </TemplateHeroSideBar>
             <TemplateMain>
-                <h1 className='font-bold text-3xl'>Cadastrar nova despesa</h1>
+                <h1 className='font-bold text-center text-3xl'>Cadastrar nova despesa</h1>
                 <form className='flex flex-col w-full gap-4 max-w-xs'>
                     <InputText label='Nome da despesa'  type='text' autoComplete='none'/>
                     <InputText label='Categiria'  type='text' autoComplete='none'/>
